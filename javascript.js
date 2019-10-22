43ae0e39eada24c81a288ce559779f4501908467
simonesciacovelli <scias758@newschool.edu>
2019-10-22 09:05:24 -0400

    h

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+// $(document).ready(function () {
+//     $(".anim1").hover(function () {
+//         $(this).children("video")[0].play();
+//     }, function () {
+//         var el = $(this).children("video")[0];
+//         el.pause();
+//         el.currentTime = 0;
+//     });
+// });
